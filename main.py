e2f32cbe41433ae9b96c91e2fa2fd2217b81119d
Kudeli4Toma <kudeli4toma@gmail.com>
2024-07-04 17:43:19 +0300

    Add files via upload
    Lesson-3, Переменные.

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-a = "Conservatory"
-print(a[:1])
-print(a[-1])
-print(a[5:])
-print(a[::-1])
-print(a[1:12:2])
+Home_work = 12
+
+Total_time = 1.5
+
+Lesson_name = 'Python'
+
+mean = (Total_time/Home_work)
+
+print(Lesson_name, 'Всего задач', ':', Home_work, ',', 'Затрачено часов', ':', Total_time, ',', mean)
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
